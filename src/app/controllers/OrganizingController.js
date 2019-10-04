f9ee5d49d9080723d4ae1ca30fb96430b1682f6d
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-10-04 20:42:41 -0300

    Adiciona paginação na lista de Meetups Organizados

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -3,10 +3,13 @@ import Meetup from '../models/Meetup';
 
 class OrganizingController {
   async index(req, res) {
+    const page = req.query.page || 1;
     const meetups = await Meetup.findAll({
       where: {
         user_id: req.userId,
       },
+      limit: 10,
+      offset: (page - 1) * 10,
     });
 
     return res.json(meetups.sort((a, b) => compareAsc(a.date, b.date)));
