beba2c4d2750f87ed9f95b3189bf8e532527c83c
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-10-04 23:15:30 -0300

    Inclui arquio no retorno da Organização

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,5 +1,6 @@
-import { compareAsc } from 'date-fns';
+import { compareAsc, isBefore, parseISO } from 'date-fns';
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class OrganizingController {
   async index(req, res) {
@@ -8,8 +9,12 @@ class OrganizingController {
       where: {
         user_id: req.userId,
       },
+      include: [File],
       limit: 10,
       offset: (page - 1) * 10,
+    }).map(meetup => {
+      meetup.past = isBefore(parseISO(meetup.date), new Date());
+      return meetup;
     });
 
     return res.json(meetups.sort((a, b) => compareAsc(a.date, b.date)));
